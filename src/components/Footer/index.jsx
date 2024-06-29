275691b8c294a540870dadf952dc39c31920dcdb
pejuameenat <104262132+pejuameenat@users.noreply.github.com>
2024-06-29 14:21:30 +0100

    footer list hover state

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bg-slate-900">
       <div className="containerWidth">
-        <div className="bg-orange-600 w-10/12 rounded-lg lg:w-3/6 h-20 mx-auto p-3 relative top-10">
-          <div className="bg-amber-400 w-16 h-16 rounded-br-full rounded-tl-3xl absolute top-0 left-0 -z-1"></div>
+        <div className="bg-orange-600 w-10/12 flex justify-center items-center rounded-lg lg:w-3/6 h-24 mx-auto p-3 relative top-10">
+          <div className="bg-amber-400 w-16 h-20 rounded-br-full rounded-tl-3xl absolute top-0 left-0 -z-1"></div>
           <p className="text-white text-center">
             Lets get connected and start finding your dream job
           </p>
